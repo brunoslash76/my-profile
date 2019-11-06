1bb1a56881966fcc409b3d3881a775a3ca3188bf
Bruno Russo <bruno.russo@centauro.com.br>
2019-11-06 17:58:29 -0300

    [WIP] Feature: Initial state

---
+++ index.js
@@ -0,0 +1,7 @@
+const navBar = document.querySelector('.nav-bar');
+
+navBar.addEventListener('mouseenter', linkUnderlineAnimation);
+
+function linkUnderlineAnimation() {
+    console.log(this);
+}
